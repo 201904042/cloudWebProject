2390979a6472da073108368d3bfecf488f3071e0
PSH <127812627+201904042@users.noreply.github.com>
2024-05-30 17:08:39 +0900

    Update BookDTO.java
    앞에 book 제거

--- BookReview/src/main/java/com/example/BookReview/BookDTO.java
+++ BookReview/src/main/java/com/example/BookReview/BookDTO.java
@@ -5,10 +5,10 @@ import lombok.*;
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter @ToString
 public class BookDTO {
-    long bookId;
-    String bookTitle;
-    String bookAuthor;
-    String bookGenre;
-    String bookSummary;
-    int bookReviewCount;
+    long id;
+    String title;
+    String author;
+    String genre;
+    String summary;
+    int reviewCount;
 }
