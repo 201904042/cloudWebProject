d67550889940f457a61a215d32685077b097d408
201904042 <packtty18@naver.com>
2024-05-30 11:43:10 +0900

    Update BookReivewDAOImpl.java
    책과 리뷰 데이터베이스 리스트 생성

--- BookReview/src/main/java/com/example/BookReview/BookReivewDAOImpl.java
+++ BookReview/src/main/java/com/example/BookReview/BookReivewDAOImpl.java
@@ -1,4 +1,18 @@
 package com.example.BookReview;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookReivewDAOImpl implements BookReviewDAO{
+    private List<BookDTO> bookList;
+    private List<ReviewDTO> reviewList;
+    public BookReivewDAOImpl(){
+        bookList = new ArrayList<>();
+        reviewList = new ArrayList<>();
+    }
+
+
+
+
 }
